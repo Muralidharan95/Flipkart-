461a83723482787218e5d2d8903eb56f3d05474b
Muralidharan95 <muralipandiyan98@gmail.com>
2023-01-22 16:47:54 +0530

    Overthinking

---
+++ src/main/java/org/nms/CodeApache.java
@@ -0,0 +1,18 @@
+package org.nms;
+
+public class CodeApache {
+	
+	private void tc1() {
+		System.out.println("live");
+
+	}
+	private void tc2() {
+		System.out.println("your");
+
+	}
+	private void tc3() {
+		System.out.println("life");
+
+	}
+
+}
